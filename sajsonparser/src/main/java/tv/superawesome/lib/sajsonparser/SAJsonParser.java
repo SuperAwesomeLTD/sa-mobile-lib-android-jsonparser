1d9b59f1876f924a8a957916d7bafd998cd33924
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-07-29 09:54:20 +0100

    0.1.3

--- sajsonparser/src/main/java/tv/superawesome/lib/sajsonparser/SAJsonParser.java
+++ sajsonparser/src/main/java/tv/superawesome/lib/sajsonparser/SAJsonParser.java
@@ -29,6 +29,12 @@ public class SAJsonParser {
         }
     }
 
+    /**
+     * Create a JSON object more elegantly
+     * @param args an array of key-value pairs given as
+     *             new Object[] { "key1", val1, "key2", val2 }
+     * @return a valid JSONObject (or an empty one)
+     */
     public static JSONObject create (Object[] args) {
         JSONObject jsonObject = new JSONObject();
         if (args.length % 2 != 0) return jsonObject;
@@ -62,11 +68,37 @@ public class SAJsonParser {
                 } catch (JSONException e) {
                     return null;
                 }
-            }
+            } else return null;
         }
-        return null;
     }
 
+    /**
+     * Standard get w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @return the return type -
+     */
+    public static Object get(@Nullable JSONObject jsonObject, @Nullable String key, @Nullable Object def) {
+        if (jsonObject == null) {
+            return def;
+        }
+        else {
+            if (!jsonObject.isNull(key)) {
+                try {
+                    return jsonObject.get(key);
+                } catch (JSONException e) {
+                    return def;
+                }
+            } else return def;
+        }
+    }
+
+    /**
+     * Get a specific string
+     * @param jsonObject target json object
+     * @param key the key
+     * @return a string value from the JSON
+     */
     @Nullable
     public static String getString(@Nullable JSONObject jsonObject, @Nullable String key) {
         if (jsonObject == null) {
@@ -79,23 +111,79 @@ public class SAJsonParser {
                 } catch (JSONException e) {
                     return null;
                 }
-            }
-        } return null;
+            } else return null;
+        }
     }
 
+    /**
+     * Get a specific string value w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @param def the default string to return
+     * @return a string value from the JSON
+     */
+    public static String getString(@Nullable JSONObject jsonObject, @Nullable String key, @Nullable String def) {
+        if (jsonObject == null) {
+            return def;
+        }
+        else {
+            if (!jsonObject.isNull(key)) {
+                try {
+                    return jsonObject.getString(key);
+                } catch (JSONException e) {
+                    return def;
+                }
+            } else return def;
+        }
+    }
+
+    /**
+     * Get a specific boolean value
+     * @param jsonObject target json object
+     * @param key the key
+     * @return a boolean value from the JSON
+     */
     public static boolean getBoolean(@Nullable JSONObject jsonObject, @Nullable String key) {
         if (jsonObject == null) {
             return false;
+        } else {
+            if (!jsonObject.isNull(key)) {
+                try {
+                    return jsonObject.getBoolean(key);
+                } catch (JSONException e) {
+                    return false;
+                }
+            } else return false;
         }
-        else if (!jsonObject.isNull(key)) {
-            try {
-                return jsonObject.getBoolean(key);
-            } catch (JSONException e) {
-                return false;
-            }
-        } return false;
     }
 
+    /**
+     * * Get a specific boolean value w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @param def the default boolean to return
+     * @return a boolean value from the JSON
+     */
+    public static boolean getBoolean(@Nullable JSONObject jsonObject, @Nullable String key, boolean def) {
+        if (jsonObject == null) {
+            return def;
+        } else {
+            if (!jsonObject.isNull(key)) {
+                try {
+                    return jsonObject.getBoolean(key);
+                } catch (JSONException e) {
+                    return def;
+                }
+            } else return def;
+        }
+    }
+
+    /**
+     * Get a specific integer value
+     * @param jsonObject target json object
+     * @param key the key
+     * @return an integer value from the JSON
+     */
     public static int getInt(@Nullable JSONObject jsonObject, @Nullable String key) {
         if (jsonObject == null) {
             return 0;
@@ -107,10 +195,38 @@ public class SAJsonParser {
                 } catch (JSONException e) {
                     return 0;
                 }
-            }
-        } return 0;
+            } else return 0;
+        }
     }
 
+    /**
+     * Get a specific integer value w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @param def the default integer to return
+     * @return a integer value from the JSON
+     */
+    public static int getInt(@Nullable JSONObject jsonObject, @Nullable String key, int def) {
+        if (jsonObject == null) {
+            return def;
+        }
+        else {
+            if (!jsonObject.isNull(key)) {
+                try {
+                    return jsonObject.getInt(key);
+                } catch (JSONException e) {
+                    return def;
+                }
+            } else return def;
+        }
+    }
+
+    /**
+     * Get a specific double value
+     * @param jsonObject target json object
+     * @param key the key
+     * @return a double value from the JSON
+     */
     public static double getDouble(@Nullable JSONObject jsonObject, @Nullable String key) {
         if (jsonObject == null) {
             return 0;
@@ -121,10 +237,37 @@ public class SAJsonParser {
                 } catch (JSONException e) {
                     return 0;
                 }
-            }
-        } return 0;
+            } else return 0;
+        }
     }
 
+    /**
+     * Get a specific double value w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @param def the default double to return
+     * @return a double value from the JSON
+     */
+    public static double getDouble(@Nullable JSONObject jsonObject, @Nullable String key, double def) {
+        if (jsonObject == null) {
+            return def;
+        } else {
+            if (!jsonObject.isNull(key)) {
+                try {
+                    return jsonObject.getDouble(key);
+                } catch (JSONException e) {
+                    return def;
+                }
+            } else return def;
+        }
+    }
+
+    /**
+     * Get a specific long value w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @return a long value from the JSON
+     */
     public static long getLong(@Nullable JSONObject jsonObject, @Nullable String key) {
         if (jsonObject == null) {
             return 0;
@@ -135,10 +278,37 @@ public class SAJsonParser {
                 } catch (JSONException e) {
                     return 0;
                 }
-            }
-        } return 0;
+            } else return 0;
+        }
     }
 
+    /**
+     * Get a specific long value w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @param def the default long to return
+     * @return a long value from the JSON
+     */
+    public static long getLong(@Nullable JSONObject jsonObject, @Nullable String key, long def) {
+        if (jsonObject == null) {
+            return def;
+        } else {
+            if (!jsonObject.isNull(key)) {
+                try {
+                    return jsonObject.getLong(key);
+                } catch (JSONException e) {
+                    return def;
+                }
+            } else return def;
+        }
+    }
+
+    /**
+     * Get a specific json array value w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @return a json array value from the JSON
+     */
     @Nullable
     public static JSONArray getJsonArray(@Nullable JSONObject jsonObject, @Nullable String key) {
         if (jsonObject == null) {
@@ -150,10 +320,38 @@ public class SAJsonParser {
                 } catch (JSONException e) {
                     return null;
                 }
-            }
-        } return null;
+            } else return null;
+        }
     }
 
+    /**
+     * Get a specific json array value w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @param def the default json array to return
+     * @return a json array value from the JSON
+     */
+    @Nullable
+    public static JSONArray getJsonArray(@Nullable JSONObject jsonObject, @Nullable String key, @Nullable JSONArray def) {
+        if (jsonObject == null) {
+            return def;
+        } else {
+            if (!jsonObject.isNull(key)) {
+                try {
+                    return jsonObject.getJSONArray(key);
+                } catch (JSONException e) {
+                    return def;
+                }
+            } else return def;
+        }
+    }
+
+    /**
+     * Get a specific json object value w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @return a json object value from the JSON
+     */
     @Nullable
     public static JSONObject getJsonObject(@Nullable JSONObject jsonObject, @Nullable String key) {
         if (jsonObject == null) {
@@ -166,8 +364,30 @@ public class SAJsonParser {
                 } catch (JSONException e) {
                     return null;
                 }
-            } return null;
+            } else return null;
         }
     }
 
+    /**
+     * Get a specific json object value w/ a default value
+     * @param jsonObject target json object
+     * @param key the key
+     * @param def the default json object to return
+     * @return a json object value from the JSON
+     */
+    @Nullable
+    public static JSONObject getJsonObject(@Nullable JSONObject jsonObject, @Nullable String key, @Nullable JSONObject def) {
+        if (jsonObject == null) {
+            return def;
+        }
+        else  {
+            if (!jsonObject.isNull(key)) {
+                try {
+                    return jsonObject.getJSONObject(key);
+                } catch (JSONException e) {
+                    return def;
+                }
+            } else return def;
+        }
+    }
 }
